9844dc3b259778170e472d313c7ee14f43de6026
SaiHemanthBR <sh.bheemreddy@icloud.com>
2020-10-01 14:50:36 +0530

    Added app.config.ts

---
+++ app-template.config.ts
@@ -0,0 +1,16 @@
+// rename this file to app.config.ts
+// Create a Firebase Project, add a web app in the project and config below
+
+export default {
+  name: 'Todo',
+  version: '1.0.0',
+  extra: {
+    apiKey: "###API_KEY###",
+    authDomain: "###AUTH_DOMAIN###",
+    databaseURL: "###DATABASE_URL###",
+    projectId: "###PROJECT_ID###",
+    storageBucket: "###STORAGE_BUCKET###",
+    messagingSenderId: "###MESSAGING_SENDER_ID###",
+    appId: "###APP_ID###",
+  },
+};
